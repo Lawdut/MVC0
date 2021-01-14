45a73d620e791b2a598291984d872bd292f04301
Lawdut <tprigent@hotmail.fr>
2021-01-14 16:20:34 +0100

    first commit

---
+++ template/template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title ; ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    </head>
+
+    <body>
+        <header>
+        <nav>
+        
+            <ul>
+                <li><a href="index.php?page=accueil">Accueil</a></li>
+                <li><a href="index.php?page=test">Test</a></li>
+                <li><a href="index.php?page=about">A propos</a></li>
+                <li><a href="index.php?page=lorem">Loremp Ipsum</a></li>
+            </ul>
+
+            </nav>
+            <h1>Titre du site</h1>
+            <p>Less is more</p>
+        </header>
+        <article>
+            
+            <?php echo $contenu ; ?>
+
+        </article>
+        <footer>
+            Designed by me
+        </footer>
+    </body>
+</html>
